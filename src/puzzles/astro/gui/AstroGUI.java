c33854587ea87135354aac6508c28101521e2a4c
Quang <20378quang@gmail.com>
2024-04-16 10:57:14 -0400

    04/16/24 10:57am commented astro gui

--- src/puzzles/astro/gui/AstroGUI.java
+++ src/puzzles/astro/gui/AstroGUI.java
@@ -4,13 +4,15 @@ import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
+import puzzles.astro.model.AstroModel;
 import puzzles.common.Observer;
 import puzzles.hoppers.model.HoppersModel;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class AstroGUI extends Application implements Observer<HoppersModel, String> {
+public class AstroGUI extends Application implements Observer<AstroModel, String> {
+    private AstroModel model;
     /** The resources directory is located directly underneath the gui package */
     private final static String RESOURCES_DIR = "resources/";
 
@@ -22,10 +24,18 @@ public class AstroGUI extends Application implements Observer<HoppersModel, Stri
     /** The size of all icons, in square dimension */
     private final static int ICON_SIZE = 75;
 
+    /**
+     * Initialize AstroModel with getting filename and add observer
+     */
     public void init() {
         String filename = getParameters().getRaw().get(0);
     }
 
+    /**
+     * Construct the layout for Astro
+     *
+     * @param stage container (window) in which to render the GUI
+     */
     @Override
     public void start(Stage stage) throws Exception {
         Button button = new Button();
@@ -39,10 +49,23 @@ public class AstroGUI extends Application implements Observer<HoppersModel, Stri
         stage.show();
     }
 
+    /**
+     * Called by the model, model.AstroModdel, whenever there is a state change
+     * that needs to be updated by the GUI.
+     *
+     * @param astroModel the AstroModel
+     * @param msg the status message sent by the model
+     */
     @Override
-    public void update(HoppersModel hoppersModel, String msg) {
+    public void update(AstroModel astroModel, String msg) {
+        // todo
     }
 
+    /**
+     * Run the main program
+     *
+     * @param args arguments
+     */
     public static void main(String[] args) {
         if (args.length != 1) {
             System.out.println("Usage: java AstroGUI filename");
