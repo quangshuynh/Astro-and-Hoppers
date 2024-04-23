bee0e2b67dcc68841a7db0c7c2c2ee0851e2d640
Quang <20378quang@gmail.com>
2024-04-23 10:03:40 -0400

    04/23/24 10:03am fixed astro spacing

--- src/puzzles/astro/solver/Astro.java
+++ src/puzzles/astro/solver/Astro.java
@@ -29,7 +29,7 @@ public class Astro {
         System.out.println("Unique configs: " + solver.getUniqueConfigs());
         if(solution != null && !solution.isEmpty()) {  // print steps
             for(int stepNum = 0; stepNum < solution.size(); stepNum++) {
-                System.out.println("Step " + stepNum + ":\n" + solution.get(stepNum));
+                System.out.println("Step " + stepNum + ": \n" + solution.get(stepNum) + "\n");
             }
         } else {
             System.out.println("No solution");
